e2a8eefd681740f4c68eb8301acabcfb07106a85
OriginalKilljoy <162512500+OriginalKilljoy@users.noreply.github.com>
2024-12-03 15:08:09 +0100

    Add project files.

---
+++ TextRPG_OwnProject/Program.cs
@@ -0,0 +1,16 @@
+﻿/*TEXT RPG
+Lag lage en karakter med klasse, equipment osv, hvor man kan gjøre oppdrag og tjene penger.
+En liten fantasiverden som kommer til live med forklaring og hovedpersonens valg i forhold til hvor en skal reise,
+hvilken oppdrag en takker ja til, og hvem en angriper.
+
+
+[]Main character
+[]Environment - Bulletin board, tavern, shops, NPCs
+[]Quests - receive money, receive reputation
+[]Villains
+[]Backpack
+*/
+
+Console.WriteLine("Welcome welcome traveller, to the world of Ceesharp!\n" +
+                  "Something tells me you're in the mood for an adventure, yes?\n" +
+                  "Before we begin, what is your name?\n");
